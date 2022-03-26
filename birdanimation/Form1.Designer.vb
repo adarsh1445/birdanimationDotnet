4491d4d9da3baa02afe7a6f3580287f1310a2e8f
adarsh144 <adarshkrishnaa1310@gmail.com>
2022-03-26 07:54:46 +0530

    k

---
+++ birdanimation/Form1.Designer.vb
@@ -0,0 +1,82 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Pic = New System.Windows.Forms.PictureBox()
+        Me.Pic1 = New System.Windows.Forms.PictureBox()
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.Pic, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Pic1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 300
+        '
+        'Pic
+        '
+        Me.Pic.Image = Global.birdanimation.My.Resources.Resources.i1
+        Me.Pic.Location = New System.Drawing.Point(270, 116)
+        Me.Pic.Name = "Pic"
+        Me.Pic.Size = New System.Drawing.Size(95, 98)
+        Me.Pic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.Pic.TabIndex = 0
+        Me.Pic.TabStop = False
+        '
+        'Pic1
+        '
+        Me.Pic1.Image = Global.birdanimation.My.Resources.Resources.i2
+        Me.Pic1.Location = New System.Drawing.Point(455, 155)
+        Me.Pic1.Name = "Pic1"
+        Me.Pic1.Size = New System.Drawing.Size(90, 93)
+        Me.Pic1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.Pic1.TabIndex = 1
+        Me.Pic1.TabStop = False
+        '
+        'Timer2
+        '
+        Me.Timer2.Interval = 250
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Pic1)
+        Me.Controls.Add(Me.Pic)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.Pic, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Pic1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Pic As PictureBox
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Pic1 As PictureBox
+    Friend WithEvents Timer2 As Timer
+End Class
---
+++ birdanimation/Form1.vb
@@ -0,0 +1,52 @@
+﻿Public Class Form1
+    Dim bird As Integer = 1
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Select Case bird
+            Case 1
+                Pic.Image = My.Resources.i1
+                bird += 1
+            Case 2
+                Pic.Image = My.Resources.i2
+                bird += 1
+            Case 3
+                Pic.Image = My.Resources.i3
+                bird += 1
+            Case 4
+                Pic.Image = My.Resources.i4
+                bird = 1
+        End Select
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Timer1.Start()
+        Timer2.Start()
+
+
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles Pic.Click
+
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        Select Case bird
+            Case 1
+                Pic1.Image = My.Resources.i2
+                bird += 1
+            Case 2
+                Pic1.Image = My.Resources.i3
+                bird += 1
+            Case 3
+                Pic1.Image = My.Resources.i4
+                bird += 1
+            Case 4
+                Pic1.Image = My.Resources.i1
+                bird = 1
+        End Select
+    End Sub
+
+    Private Sub PictureBox1_Click_1(sender As Object, e As EventArgs) Handles Pic1.Click
+
+    End Sub
+End Class
